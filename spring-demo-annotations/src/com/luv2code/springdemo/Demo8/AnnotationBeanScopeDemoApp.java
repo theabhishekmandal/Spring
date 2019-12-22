62b99231cc6f4a91bbf8e54b62fb894d3c06685d
theabhishekmandal <abhishekmandal0097@gmail.com>
2019-12-22 17:32:53 +0530

    adding comments in annotation package 22Dec2019

--- spring-demo-annotations/src/com/luv2code/springdemo/Demo8/AnnotationBeanScopeDemoApp.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/Demo8/AnnotationBeanScopeDemoApp.java
@@ -17,10 +17,68 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * 	Injecting properties file 
  * 
  * 	Adding @Scope("Singleton") in TennisCoach class and @Scope("Prototype") in BadmintonCoach class
+ * 
+ * 	Adding @PostConstruct and @PreDestroy methods
+ * 	@PreDestroy methods are not called for prototype objects 
+ */
+/*
+ *  Here through @Component Scan and using default bean id 
+ *  and using Dependency injection using @Autowired
+ *  
+ *  an @Autowired annotation on such a constructor is no longer necessary 
+ *  if the target bean only defines one constructor to begin with. However, 
+ *  if several constructors are available, at least one must be annotated to
+ *  teach the container which one to use
+ *  
+ *  Using field injection instead of setter injection and constructor injection
+ *  
+ *  Using @Qualifier to decide which dependency to use out of multiple dependencies
+ *  
+ * 	Injecting properties file 
+ * 
+ * 	Adding @Scope("Singleton") in TennisCoach class and @Scope("Prototype") in BadmintonCoach class
+ * 
+ * 	Adding @PostConstruct and @PreDestroy methods
+ * 	@PreDestroy methods are not called for prototype objects 
+ */
+/*
+ *  Here through @Component Scan and using default bean id 
+ *  and using Dependency injection using @Autowired
+ *  
+ *  an @Autowired annotation on such a constructor is no longer necessary 
+ *  if the target bean only defines one constructor to begin with. However, 
+ *  if several constructors are available, at least one must be annotated to
+ *  teach the container which one to use
+ *  
+ *  Using field injection instead of setter injection and constructor injection
+ *  
+ *  Using @Qualifier to decide which dependency to use out of multiple dependencies
+ *  
+ * 	Injecting properties file 
+ * 
+ * 	Adding @Scope("Singleton") in TennisCoach class and @Scope("Prototype") in BadmintonCoach class
  */
 
 import com.luv2code.springdemo.Interface.Coach;
 
+/*
+ *  Here through @Component Scan and using default bean id 
+ *  and using Dependency injection using @Autowired
+ *  
+ *  an @Autowired annotation on such a constructor is no longer necessary 
+ *  if the target bean only defines one constructor to begin with. However, 
+ *  if several constructors are available, at least one must be annotated to
+ *  teach the container which one to use
+ *  
+ *  Using field injection instead of setter injection and constructor injection
+ *  
+ *  Using @Qualifier to decide which dependency to use out of multiple dependencies
+ *  
+ * 	Injecting properties file 
+ * 
+ * 	Adding @Scope("Singleton") in TennisCoach class and @Scope("Prototype") in BadmintonCoach class
+ * 
+ */
 public class AnnotationBeanScopeDemoApp {
 
 	public static void main(String[] args) {
